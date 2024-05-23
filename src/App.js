8c9e098c2ccf638c64af89ed202bbc99be39a74f
Biwott362 <biwottttt@gmail.com>
2024-05-23 15:50:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
 const API_URL = 'http://www.omdbapi.com?apikey=854c4f4f';
-const movie =
-    {
-        "Title": "Wonder Woman",
-        "Year": "2017",
-        "imdbID": "tt0451279",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BMTYzODQzYjQtNTczNC00MzZhLTg1ZWYtZDUxYmQ3ZTY4NzA1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
-    }
 
 function App() {
 
