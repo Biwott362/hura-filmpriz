ffb0f3b5868d6b7a9cbe75fadaba2b4003ee59c2
Biwott362 <biwottttt@gmail.com>
2024-05-23 16:13:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
-const API_URL = 'http://www.omdbapi.com?apikey=854c4f4f';
+const API_URL = 'https://www.omdbapi.com?apikey=854c4f4f';
 
 function App() {
 
